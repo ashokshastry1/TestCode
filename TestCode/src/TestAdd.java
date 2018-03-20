b5ff5ef64c24dd2fa20324da61f92218dc93d56a
lawrencegodwin.pinto <lawrencegodwin.pinto@ELW3064.spanservices.com>
2018-03-20 12:51:44 +0530

    First Commit

---
+++ TestCode/src/TestAdd.java
@@ -0,0 +1,14 @@
+
+public class TestAdd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a = 10, b = 20, c;
+		c = a + b;
+		System.out.println("Value of c is :" +c);
+			
+		
+	}
+
+}
