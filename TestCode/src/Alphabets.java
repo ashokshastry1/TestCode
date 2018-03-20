5d295e416102ed9a6dd60630452801b9fd7fd2e0
lawrencegodwin.pinto <lawrencegodwin.pinto@ELW3064.spanservices.com>
2018-03-20 17:28:44 +0530

    New code

---
+++ TestCode/src/Alphabets.java
@@ -0,0 +1,15 @@
+
+public class Alphabets {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i;
+		for (i = 65; i <= 90; i++) {
+			System.out.println(("The Alphabet is:" +(char)i));
+			
+		}		
+
+	}
+
+}
