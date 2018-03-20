513c03df5e36ab7c12d2b1ab07d5d56aa8b087fa
lawrencegodwin.pinto <lawrencegodwin.pinto@ELW3064.spanservices.com>
2018-03-20 17:36:51 +0530

    changes

--- TestCode/src/Alphabets.java
+++ TestCode/src/Alphabets.java
@@ -5,8 +5,8 @@ public class Alphabets {
 		// TODO Auto-generated method stub
 		
 		int i;
-		for (i = 65; i <= 90; i++) {
-			System.out.println(("The Alphabet is:" +(char)i));
+		for (i = 65; i <= 70; i++) {
+			System.out.println(("The Alphabet is : " +(char)i));
 			
 		}		
 
