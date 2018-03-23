287b49a53fac897a8343ebd77b17d4c04c3bd538
lawrencegodwin.pinto <lawrencegodwin.pinto@ELW3064.spanservices.com>
2018-03-23 11:42:12 +0530

    new code

---
+++ TestCode/src/Maths.java
@@ -0,0 +1,17 @@
+
+public class Maths {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 100; int b = 200; int c = 400; int d; int e; int f;
+		
+		d = a + b + c;
+		System.out.println("Sum is 				:	" +d);
+		e = d/3;
+		System.out.println("Average is 			: 	" +e);
+		f = a * b * c;
+		System.out.println("Multiplication is 		: 	" +f);
+	}
+
+}
